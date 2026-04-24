45783e128b1351c9f061c0ae7ea259a6b1fcae61
simon <simon@yint.dev>
2026-04-24 14:47:16 +0200

    ck/configuration-management
    Signed-off-by: simon <simon@yint.dev>

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ import { logger } from "@hono/hono/logger";
 
 const app = new Hono();
 
+const message = Deno.env.get("WELCOME_MESSAGE") || "Hello world!";
+
 app.use("/*", cors());
 app.use("/*", logger());
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello world!" });
-});
+app.get("/", (c) => c.json({ message }));
 
 export default app;
